df855cb5d5b09821344732ffab5f15e6d7bd570a
Diogo Calgaro <diogocalgaro@gmail.com>
2014-08-11 15:52:24 -0300

    Desenvolvimento da versão 0.70.

--- boadica.sh
+++ boadica.sh
@@ -14,6 +14,9 @@ test $? -ne 0 && exit $?
 #config basica
 source $(dirname $0)/config.inc
 
+#config local
+sql_limit="limit 1000" #maximo de resultado numa query sql
+
 #funcoes
 source ${base}/funcoes.inc
 
@@ -40,14 +43,75 @@ do
 	test $? -ne 0 && break
 		
 	case "$op" in
-		"L") #lojas
-			lojas_filtro=1
-			lojas_filtro_txt="Ver todas as lojas (DESATIVAR FILTRO)"
-			lojas_filtro_sql="where o.consultar='S'"
+		"P") #produtos
+			prod_filtro=1
+			categ_filtro_item=''
+			prod_filtro_txt="Ver todos os produtos (DESATIVAR TODOS OS FILTROS)"
 			op2=""
+
 			while [ "$op2" != "V" ]
 			do
-				i=$(sqlite3 -list -separator " " ${db} "select id, quote(loja||'   ['||produtos||']') from (select q.id, q.loja, count(distinct p.produto) as produtos from (select l.id, l.nome||',   '||o.nome as loja from lojas l inner join locais o on (o.id = l.local) ${lojas_filtro_sql}) q left join precos p on (p.loja = q.id) group by 1, 2) order by 2;")
+				prod_filtro_sql="where c.id in (${categ_filtro_item}-1)"
+
+				i=$(sqlite3 -list -separator " " ${db} "select quote(c.id||';'||p.id), quote('['||c.nome||']  '||p.fabricante||'  -  '||p.nome) from produtos p inner join categorias c on (c.id = p.categoria) ${prod_filtro_sql} order by c.nome, p.fabricante, p.nome ${sql_limit};")
+				i=${i//$'\n'/ }
+				i=${i//\'/\"}
+
+				eval "op2=\$(dialog --stdout --no-cancel --no-tags \
+					--menu 'Produtos' 30 90 23 \
+					V 'Voltar' \
+					B 'Buscar por uma produto' \
+					T '"${prod_filtro_txt}"' \
+					F 'Editar filtro de categorias visíveis' \
+					'' '==============================================================================' \
+					${i})"
+
+				case "$op2" in
+					"B") #busca
+						eval "pesq=\$(dialog --stdout --title 'Consultar produto' --inputbox 'Informe o nome do fabricante ou do produto' 8 60)"
+						if [ $? -eq 0 -a -n "$pesq" ]
+						then
+							prod_filtro=1
+							categ_filtro_item='-1'
+							prod_filtro_txt="Ver todos os produtos (DESATIVAR TODOS OS FILTROS)"
+							prod_filtro_sql="where c.id in (${categ_filtro_item}-1) and (p.fabricante like '%{pesq}%' or p.nome like '%{pesq}%')"
+						fi ;;
+					"T") #ver todos
+						if [ $prod_filtro -eq 1 ]
+						then
+							prod_filtro=0
+							categ_filtro_item='-1'
+							prod_filtro_txt="Ver somente os produtos das categorias selecionadas (ATIVAR FILTRO DE CATEGORIA)"
+							prod_filtro_sql=""
+						else
+							prod_filtro=1
+							categ_filtro_item='-1'
+							prod_filtro_txt="Ver todos os produtos (DESATIVAR TODOS OS FILTROS)"
+							prod_filtro_sql="where c.id in (${categ_filtro_item}-1)'"
+						fi ;;
+					"F") #filtro
+						i=$(sqlite3 -csv -separator " " $db "select id, nome, 'off' as opcao from categorias where id <> '0' order by 2 ${sql_limit}" | tr '\n' ' ')
+						cod=""
+						eval "cod=\$(dialog --stdout --checklist 'Categorias visíveis' 30 60 23 ${i})"
+		
+						if [ $? -eq 0 ]
+						then
+							cod=${cod// /,}
+							test -n "${cod}" && cod=${cod}','
+							categ_filtro_item="${cod}"
+						fi
+					;;
+				esac
+			done ;;
+		"L") #lojas
+			lojas_filtro=1
+			lojas_filtro_txt="Ver todas as lojas (DESATIVAR TODOS OS FILTROS)"
+			lojas_filtro_sql="where o.consultar='S'"
+			op2=""
+
+			while [ "$op2" != "V" ]
+			do
+				i=$(sqlite3 -list -separator " " ${db} "select id, quote(loja||'   ['||produtos||']') from (select q.id, q.loja, count(distinct p.produto) as produtos from (select l.id, l.nome||',   '||o.nome as loja from lojas l inner join locais o on (o.id = l.local) ${lojas_filtro_sql}) q left join precos p on (p.loja = q.id) group by 1, 2) order by 2 ${sql_limit};")
 				i=${i//$'\n'/ }
 				i=${i//\'/\"}
 
@@ -62,37 +126,22 @@ do
 
 				case "$op2" in
 					"B") #buscar loja
-# APROVEITAR ESSA PARTE DO CODIGO ANTERIOR
-#						l=$(whiptail --title "Consultar Loja" --inputbox "Informe o nome da loja" $alt_jan_pq $larg_jan_pq 3>&2 2>&1 1>&3)
-#						if [ $? -eq 0 -a -n "$l" ]
-#						then
-#							i=$(sqlite3 -csv $db "select id, nome||' ('||localizacao||')' as item from tb_lojas where nome like '%${l}%';" | tr '\n' ' ' | tr ',' ' ')
-#
-#							eval "cod=\$(whiptail --notags --menu 'Selecione a loja' $alt_jan_gr $larg_jan_gr $menu_jan_gr ${i} 3>&2 2>&1 1>&3)"
-#							if [ -n "$cod" ]
-#							then
-#								naveg_cmd="$(which $naveg_opcoes xdg-open 2>/dev/null | head -n1)"
-#								$naveg_cmd "${pag_vendedor}${cod}" >/dev/null 2>&1 &
-#							fi
-#						fi
-#						p=$(dialog --title "Consultar loja" --inputbox "Informe o nome da loja" $alt_jan_pq $larg_jan_pq 3>&2 2>&1 1>&3)
-#						if [ $? -eq 0 -a -n "$p" ]
-#						then
-#							tmp=$(mktemp)
-#							sqlite3 -cmd ".width 20 35 8 17 10" -header -column $db "select * from vw_buscar_por_loja where Loja like '%${p}%';" > $tmp
-#							whiptail --title "Consultar itens da loja" --scrolltext --textbox $tmp $alt_jan_gr $larg_jan_gr
-#							rm $tmp
-#						fi ;;
-;;
+						eval "pesq=\$(dialog --stdout --title 'Consultar loja' --inputbox 'Informe o nome da loja' 8 60)"
+						if [ $? -eq 0 -a -n "$pesq" ]
+						then
+							lojas_filtro=1
+							lojas_filtro_txt="Ver todas as lojas (DESATIVAR FILTRO DE BUSCA)"
+							lojas_filtro_sql="where l.nome like '%$pesq%'"
+						fi ;;
 					"T") #ver todas as lojas, sem filtro
 						if [ $lojas_filtro -eq 1 ]
 						then
 							lojas_filtro=0
-							lojas_filtro_txt="Ver somente as lojas dos bairros selecionados (ATIVAR FILTRO)"
+							lojas_filtro_txt="Ver somente as lojas dos bairros selecionados (ATIVAR FILTRO DE BAIRROS)"
 							lojas_filtro_sql=""
 						else
 							lojas_filtro=1
-							lojas_filtro_txt="Ver todas as lojas (DESATIVAR FILTRO)"
+							lojas_filtro_txt="Ver todas as lojas (DESATIVAR TODOS OS FILTROS)"
 							lojas_filtro_sql="where o.consultar='S'"
 						fi ;;
 					"F") #editar filtro de bairro
@@ -100,7 +149,7 @@ do
 						cod=""
 						eval "cod=\$(dialog --stdout --checklist 'Locais visíveis' 30 60 23 ${i})"
 		
-						if [ "$?" -eq 0 ]
+						if [ $? -eq 0 ]
 						then
 							cod="$(echo $cod | sed 's/ /,/g;s/"//g')"
 							sql="update locais set consultar = 'N'; update locais set consultar='S' where id in (${cod});"
@@ -110,19 +159,25 @@ do
 						sql="select count(*) from locais where consultar='S';"
 						res=$(sqlite3 ${db} "${sql}")
 						dialog --msgbox "Localidades incluídas nas pesquisas: ${res:-0}" 7 50 ;;
+					*) #ver loja
+						if [ "$op2" != "V" ]
+						then
+							naveg_cmd="$(which $naveg_opcoes xdg-open 2>/dev/null | head -n1)"
+							$naveg_cmd "${pag_vendedor}${op2}" >/dev/null 2>&1 &
+						fi ;;
 				esac
 			done ;;
 		"C") #categorias
 			op2=""
 			while [ "$op2" != "V" ]
 			do
-				i=$(sqlite3 -list -separator " " ${db} "select id, quote(nome||' ['||total||']') from (select c.id, c.nome, count(p.id) as total from categorias c left join produtos p on (p.categoria = c.id) where c.id <> '0' group by 1, 2 order by c.nome);")
+				i=$(sqlite3 -list -separator " " ${db} "select id, quote(nome||' ['||total||']') from (select c.id, c.nome, count(p.id) as total from categorias c left join produtos p on (p.categoria = c.id) where c.id <> '0' group by 1, 2 order by c.nome) ${sql_limit};")
 				i=${i//$'\n'/ }
 				i=${i//\'/\"}
 
 				eval "op2=\$(dialog --stdout \
 					--no-cancel \
-					--menu 'Categorias selecionadas' 30 60 23 \
+					--menu 'Categorias selecionadas' 30 70 23 \
 					V 'Voltar' \
 					I 'Incluir nova categoria' \
 					T 'Atualizar todas' \
